bd137ff05bb9fb047562bb5e9ff11e9674f93059
Om Rana <97846530+OmRanAlot@users.noreply.github.com>
2024-01-22 20:06:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const apiKey = process.env.API_KEY;
-console.log(apiKey);
 
 let button = document.getElementById("testButton");
 let text = document.getElementById("change");
 button.addEventListener('click',function(){
     text.innerHTML= "I CHANGED THE TEXT WOOO";
+    console.log(apiKey);
 
 });
