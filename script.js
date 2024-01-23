6fa235456bda332833b5649dcf0aaf47dfe3c523
Om Rana <97846530+OmRanAlot@users.noreply.github.com>
2024-01-22 20:24:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+console.log("working");
+
 const apiKey = process.env.API_KEY;
 console.log("working");
+
 let button = document.getElementById("testButton");
 let text = document.getElementById("change");
 button.addEventListener('click',function(){
