41855042e7d21b6132943fc41034c1d4993252c5
Om Rana <97846530+OmRanAlot@users.noreply.github.com>
2024-01-22 19:58:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
 let button = document.getElementById("testButton");
 let text = document.getElementById("change");
 button.addEventListener('click',function(){
-    text.innerHTML= "I CHANGED THE TEXT WOOO"
+    text.innerHTML= "I CHANGED THE TEXT WOOO";
 
 });
